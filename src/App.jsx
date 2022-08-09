05c9e3dc28f0e2fd5609425cd739178ad822743b
byKriz <cristianesquivelh@hotmail.com>
2022-08-09 13:35:57 -0400

    limpiando el proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
 
   return (
-    <div className="text-3xl font-bold underline">
-      <h1>Hello World</h1>
-    </div>
-  )
+    <>
+    
+    </>
+  );
 }
 
-export default App
+export default App;
