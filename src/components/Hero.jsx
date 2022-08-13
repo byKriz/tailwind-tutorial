2b5f72ab61f667bf53a4f76b70a3a14b8ff9724a
byKriz <cristianesquivelh@hotmail.com>
2022-08-13 10:53:08 -0400

    agregando los iconos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import {
   CloudUploadIcon,
   DatabaseIcon,
   ServerIcon,
+  PaperAirplaneIcon,
 } from "@heroicons/react/solid";
 import bgImg from "../assets/cyber-bg.png";
 
@@ -10,11 +11,33 @@ export function Hero() {
   return (
     <div className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
       <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-2xl">Unique Sequencing & Production</p>
+          <h1 className="py-3 text-5xl md:text-7xl font-bold">
+            Cloud Managment
+          </h1>
+          <p className="text-2xl">This is our Teach Brand.</p>
+          <button className="py-3 px-6 sm:w-[60%]   my-4">Get Started</button>
+        </div>
         <div>
-          <p>Unique Sequencing & Production</p>
-          <h1>Cloud Managment</h1>
-          <p>This is our Teach Brand.</p>
-          <button>Get Started</button>
+          <img className="w-full" src={bgImg} alt="/" />
+        </div>
+        <div className=" absolute flex flex-col py-8 md:min-w-[760px] bottom-[-5%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl">
+          <p>Data Services</p>
+          <div className="flex justify-between flex-wrap px-4">
+            <p className="flex px-4 py-2 text-slate-500">
+              <CloudUploadIcon className="h-6 text-indigo-600" /> App Security
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <DatabaseIcon className="h-6 text-indigo-600" /> Dashboard Design
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <ServerIcon className="h-6 text-indigo-600" /> Cloud Data
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <PaperAirplaneIcon className="h-6 text-indigo-600" /> API
+            </p>
+          </div>
         </div>
       </div>
     </div>
