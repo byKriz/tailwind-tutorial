3ad4d62bceb02e997800a4701ccb688500f2ad8e
byKriz <cristianesquivelh@hotmail.com>
2022-08-16 17:21:24 -0400

    terminando el footer

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -10,7 +10,7 @@ import { ItemsBox } from "../components/ItemsBox";
 
 export const Footer = () => {
   return (
-    <div className="w-full mt-24 bg-slate-900 text-gray-300 py-6 px-2">
+    <footer className="w-full mt-24 bg-slate-900 text-gray-300 py-2 px-2">
       <div className="max-x-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8">
         <ItemsBox />
         <ItemsBox
@@ -25,7 +25,28 @@ export const Footer = () => {
           tittle="Legal"
           items={["Claims", "Privacy", "Terms", "Policies", "Conditions"]}
         />
+        <div className="col-span-2 pt-8 md:pt-2">
+          <p className=" font-bold uppercase">Subscribe to our newsletter</p>
+          <p className="py-4">
+            The lastest news, articles and recources, sent to your inbox weekly.
+          </p>
+          <form className="flex flex-col sm:flex-row">
+            <input className="w-full p-2 mr-4 rounded-md mb-4" type="email" />
+            <button className="p-2 mb-4">Subscribe</button>
+          </form>
+        </div>
       </div>
-    </div>
+
+      <div className="flex flex-col max-w-[1240px] px-2 py-4 m-auto justify-between sm:flex-row text-gray-500">
+        <p>2022 Workflow, LLC. All rights reserved</p>
+        <div className="flex justify-between sm:max-w-[360px] text-2xl gap-2 pt-2 sm:pt-0">
+          <FaFacebook className=" hover:text-white"/>
+          <FaInstagram className=" hover:text-white"/>
+          <FaTwitter className=" hover:text-white"/>
+          <FaTwitch className=" hover:text-white"/>
+          <FaGithub className=" hover:text-white"/>
+        </div>
+      </div>
+    </footer>
   );
 };
